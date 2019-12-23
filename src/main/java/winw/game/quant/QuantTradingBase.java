42e8fb8250e8439efe7834014abe70d80f6dca72
winw <winwjava@sina.com>
2019-12-23 11:31:26 +0800

    update CSI_300_TOP

--- src/main/java/winw/game/quant/QuantTradingBase.java
+++ src/main/java/winw/game/quant/QuantTradingBase.java
@@ -13,10 +13,10 @@ public abstract class QuantTradingBase {
 	public final static String SH_GOLD_ETF = "sh518880";// 上证黄金ETF
 
 	public final static String[] CSI_300_TOP = { // 沪深300十大权重股。市值大、流动性好。
-			// 中国平安// 贵州茅台// 招商银行// 格力电器// 美的集团
-			"sh601318", "sh600519", "sh600036", "sh601166", "sz000651",
-			// 兴业银行// 五 粮 液// 伊利股份// 恒瑞医药// 中信证券
-			"sz000333", "sz000858", "sh600276", "sh600887", "sh600030" };
+			// 中国平安// 贵州茅台// 招商银行// 格力电器// 兴业银行
+			"sh601318", "sh600519", "sh600036", "sh601166", "sz000333",
+			// 美的集团// 恒瑞医药// 五 粮 液// 中信证券// 万科Ａ
+			"sz000651", "sh600276", "sz000858", "sh600887", "sh600030", "000002" };
 
 	protected String currentDate;
 
